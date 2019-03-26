efa6602b0437ec19bfecbc3e062b3bb68b05566a
priest334 <priest334@126.com>
2019-03-26 17:24:37 +0800

    use boost::shared_mutex

--- xctp/ctp_session.h
+++ xctp/ctp_session.h
@@ -3,7 +3,6 @@
 #include <map>
 #include <string>
 #include <set>
-#include <shared_mutex>
 #include <boost/shared_ptr.hpp>
 #include <boost/thread.hpp>
 #include "md/md_connector.h"
@@ -27,7 +26,7 @@ public:
 
 private:
 	std::map<std::string, std::set<std::string>> sub_ins_users_;
-	std::shared_mutex mutex_;
+	boost::shared_mutex mutex_;
 	MdConnectorPtr md_connector_;
 	boost::shared_ptr<boost::thread> md_thread_;
 	MdHistory md_history_;
